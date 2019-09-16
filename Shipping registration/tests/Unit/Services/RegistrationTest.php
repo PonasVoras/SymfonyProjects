11b7f96be514fd3584e9b97dbee0b8f8dcbfcb2e
PonasVoras <debesysmelyni@gmail.com>
2019-09-16 15:42:59 +0300

    Completed Registration class testing

--- Shipping registration/tests/Unit/Services/RegistrationTest.php
+++ Shipping registration/tests/Unit/Services/RegistrationTest.php
@@ -3,52 +3,78 @@
 namespace App\Tests\Unit\Services;
 
 use App\Entity\Order as OrderEntity;
-use App\Services\OrderRegistration\Registration as OrderRegistrationService;
+use App\Service\OrderRegistration\Handlers\HandleUpsCarrierStrategy;
+use App\Service\OrderRegistration\Registration as OrderRegistrationService;
+use App\Utils\OrderRegistrationApi\RegistrationApiHelper;
 use PHPUnit\Framework\TestCase;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
 class RegistrationTest extends TestCase
 {
-//    public function testHandleOrderRegistrationMethodShouldCallMethod()
-//    {
-//
-//    }
+    private $orderRegistrationService;
+    private $handleUpsCarrierStrategy;
+    private $orderEntityStub;
+    private $orderRegistrationApiHelperStub;
+    const EXISTING_CARRIER = 'ups';
+    const NON_EXISTING_CARRIER = 'sup';
 
-//    public function testPickHandlerMethodShouldReturnHandlerObject()
-//    {
-//        $handlerObject = $this->createOrderRegistrationService()
-//            ->pickHandler('Ups');
-//        $this->assertEquals('object', gettype($handlerObject));
-//    }
-//
-//    public function testPickHandlerMethodShouldReturnException()
-//    {
-//        $handlerObject = $this->createOrderRegistrationService()
-//            ->pickHandler('Ups');
-//        $this->assertEquals('object', gettype($handlerObject));
-//    }
+    protected function setUp (): void
+    {
+        $loggerStub = $this->createMock(LoggerInterface::class);
+        $this->handleUpsCarrierStrategy = $this->createMock(HandleUpsCarrierStrategy::class);
+        $this->orderEntityStub = $this->createMock(OrderEntity::class);
+        $this->orderRegistrationApiHelperStub = $this->createMock(RegistrationApiHelper::class);
+        $this->orderEntityStub->method('getShippingCarrierName')->willReturn(self::EXISTING_CARRIER);
+        $this->orderRegistrationService = new OrderRegistrationService(
+            $loggerStub,
+            $this->orderEntityStub,
+            $this->orderRegistrationApiHelperStub
+        );
+        $this->handleUpsCarrierStrategy
+            ->method('canHandleCarrier')
+            ->with($this->logicalOr(
+                $this->equalTo(self::EXISTING_CARRIER),
+                $this->equalTo(self::NON_EXISTING_CARRIER)
+            ))
+            ->will($this->returnCallback(array($this, 'myCallback')));
+        $this->orderRegistrationService->addStrategy($this->handleUpsCarrierStrategy);
+    }
+    public function myCallback($value){
+        if ($value == self::EXISTING_CARRIER){
+            return true;
+        } else{
+            return false;
+        }
 
-//    public function testRegisterShouldCallOrderRegistrationApiHelper()
-//    {
-//
-//    }
+    }
 
-//    public function testPickHandlerShouldReturnHandleCarrierInterfaceStrategy()
-//    {
-//
-//    }
+    public function testHandleOrderRegistrationMethodShouldCallMethod()
+    {
+        $this->orderEntityStub
+            ->expects($this->once())
+            ->method('getShippingCarrierName');
+        $this->orderRegistrationService->handleOrderRegistration();
+    }
 
-//    public function testPickHandlerShouldReturnException()
-//    {
-//
-//    }
+    public function testPickHandlerMethodShouldReturnHandlerObject()
+    {
+        $actualHandlerObject = $this->orderRegistrationService->pickHandler(self::EXISTING_CARRIER);
+        $this->assertInstanceOf(HandleUpsCarrierStrategy::class, $actualHandlerObject);
+    }
 
+    public function testPickHandlerMethodShouldReturnException()
+    {
+        $this->expectException(Exception::class);
+        $this->orderRegistrationService->pickHandler(self::NON_EXISTING_CARRIER);
+    }
 
-//    public function createOrderRegistrationService(): OrderRegistrationService
-//    {
-//        $loggerStub = $this->createMock(LoggerInterface::class);
-//        $orderRegistrationServiceStub = $this->createMock(OrderEntity::class);
-//        $orderRegistrationService = new OrderRegistrationService($loggerStub, $orderRegistrationServiceStub);
-//        return $orderRegistrationService;
-//    }
+    public function testRegisterShouldCallOrderRegistrationApiHelper()
+    {
+        $this->orderRegistrationApiHelperStub
+            ->expects($this->once())
+            ->method('forwardRequest')
+            ->with($this->isType('object'));
+        $this->orderRegistrationService->register(self::EXISTING_CARRIER);
+    }
 }
